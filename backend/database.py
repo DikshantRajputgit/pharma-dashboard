6ec9af45aa2ae2e48fddfb859535cb27ca76d8a5
Dikshant  Rajput <51286848+DikshantRajputgit@users.noreply.github.com>
2026-05-29 00:01:42 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -23,27 +23,12 @@ DATABASE_URL = os.getenv(
 if DATABASE_URL.startswith("sqlite"):
 
     engine = create_engine(
-
-        DATABASE_URL,
-
-        connect_args={
-
-            "check_same_thread": False
-
-        }
-
-    )
-
-else:
-
-    engine = create_engine(
-
-        DATABASE_URL,
-
-        pool_pre_ping=True
-
-    )
-
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {},
+    pool_pre_ping=True
+)
 # =========================
 # SESSION
 # =========================
