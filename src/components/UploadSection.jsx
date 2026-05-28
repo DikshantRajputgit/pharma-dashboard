9bf88870bc3d71253f3702c304e515a8b5e2b25b
Dikshant  Rajput <51286848+DikshantRajputgit@users.noreply.github.com>
2026-05-28 22:48:45 +0530

    Update UploadSection.jsx

--- src/components/UploadSection.jsx
+++ src/components/UploadSection.jsx
@@ -51,7 +51,7 @@ export default function UploadSection({
 
       const res = await axios.post(
 
-        "${import.meta.env.VITE_API_URL}/upload",
+        "https://pharma-dashboard-r7x3.onrender.com/upload",
 
         formData,
 
