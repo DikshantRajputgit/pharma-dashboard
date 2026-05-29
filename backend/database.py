1ab91fc47ca9bf55babbf96d1b9d7d7ad70f3a9c
Dikshant  Rajput <51286848+DikshantRajputgit@users.noreply.github.com>
2026-05-29 15:01:13 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,21 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# =========================
-# DATABASE URL
-# =========================
-
 DATABASE_URL = os.getenv(
-
     "DATABASE_URL",
-
     "sqlite:///./pharma.db"
-
 )
-
-# =========================
-# ENGINE
-# =========================
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
@@ -31,38 +20,18 @@ else:
         DATABASE_URL,
         pool_pre_ping=True
     )
-# =========================
-# SESSION
-# =========================
 
 SessionLocal = sessionmaker(
-
     autocommit=False,
-
     autoflush=False,
-
     bind=engine
-
 )
-
-# =========================
-# BASE
-# =========================
 
 Base = declarative_base()
 
-# =========================
-# DB DEPENDENCY
-# =========================
-
 def get_db():
-
     db = SessionLocal()
-
     try:
-
         yield db
-
     finally:
-
         db.close()
